1ccf987b772dcfaa3c1e3439a4a28f23fcf07c7a
iquerejeta <querejeta.azurmendi@iohk.io>
2023-03-24 17:22:47 +0100

    Add bls12-381

---
+++ src/bls12_381/mod.rs
@@ -0,0 +1,19 @@
+mod fp;
+mod fp12;
+mod fp2;
+mod fp6;
+mod g1;
+mod g2;
+mod scalar;
+mod pairings;
+mod util;
+
+pub use fp::*;
+pub use fp12::*;
+pub use fp2::*;
+pub use fp6::*;
+pub use scalar::*;
+pub use g1::*;
+pub use g2::*;
+pub use pairings::*;
+
